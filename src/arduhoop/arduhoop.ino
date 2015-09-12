ade26b70af2f5fcfbeed28ace119b599c4df4d26
Arthur Benemann <arthur.benemann@gmail.com>
2015-09-11 18:57:12 -0700

    revert to single rainbow

--- src/arduhoop/arduhoop.ino
+++ src/arduhoop/arduhoop.ino
@@ -50,7 +50,7 @@ void rainbowCycle(uint8_t wait) {
 
   for(j=0; ; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
-        strip.setPixelColor(i, Wheel(((2 * i * 256 / strip.numPixels()) - j) & 255));
+        strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels())+j) & 255));
     }
     strip.show();
     delay(wait);
