130a826670fabf07ae774522082612f5d1117201
Arthur Benemann <arthur.benemann@gmail.com>
2016-05-07 14:30:56 -0700

    add extra led functions

--- src/arduhoop/arduhoop.ino
+++ src/arduhoop/arduhoop.ino
@@ -8,37 +8,44 @@ int16_t ax, ay, az, gx, gy, gz;
 int audio;
 
 void loop() {
-  Serial.print("Time "); Serial.print(millis()); Serial.print("ms\t");
 
-  // read raw accel/gyro measurements from device
-  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-  //accelgyro.getAcceleration(&ax, &ay, &az);
-  //accelgyro.getRotation(&gx, &gy, &gz);
+    Serial.print("Time "); Serial.print(millis()); Serial.print("ms\t");
 
-  gx = abs(gx);
-  gy = abs(gy);
-  gz = abs(gz);
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
 
-  Serial.print("Acc:\t");
-  Serial.print(gx); Serial.print("\t");
-  Serial.print(gy); Serial.print("\t");
-  Serial.print(gz); Serial.print("\t");
+    gx = abs(gx);
+    gy = abs(gy);
+    gz = abs(gz);
+
+    Serial.print("Acc:\t");
+    Serial.print(gx); Serial.print("\t");
+    Serial.print(gy); Serial.print("\t");
+    Serial.print(gz); Serial.print("\t");
+
+    audio = readMic();
+    Serial.print("Audio\t"); Serial.print(audio); Serial.print("\t");
+
+    Serial.print("SW "); Serial.print(isButtonPressed() ? "[X]" : "[ ]"); Serial.print("\t");
+    Serial.print("SD_SW "); Serial.print(isSDcardPresent() ? "[X]" : "[ ]"); Serial.print("\t");
+    Serial.print("Charging "); Serial.print(isBatteryCharging() ? "[X]" : "[ ]"); Serial.print("\t");
+    Serial.print("Voltage "); Serial.print(batteryVoltage()); Serial.print("V\t");
+
+    Serial.println(" ");
+    loop_cnt++;
+
+
 
   for (int i = 0; i < led_number_pixels; i++) {
-    pixels.setPixelColor(i, ((uint8_t)(gx >> 10)), ((uint8_t)(gy >> 10)), ((uint8_t)(gz >> 10))); // Moderately bright green color.
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, Wheel(loop_cnt), Wheel(80 + loop_cnt), Wheel(160 + loop_cnt)); // Moderately bright green color.
   }
-  pixels.show(); // This sends the updated pixel color to the hardware.
-
-  audio = readMic();
-  Serial.print("Audio "); Serial.print(audio); Serial.print("\t");
-
-  Serial.print("SW "); Serial.print(isButtonPressed() ? "[X]" : "[ ]"); Serial.print("\t");
-  Serial.print("SD_SW "); Serial.print(isSDcardPresent() ? "[X]" : "[ ]"); Serial.print("\t");
-  Serial.print("Charging "); Serial.print(isBatteryCharging() ? "[X]" : "[ ]"); Serial.print("\t");
-  Serial.print("Voltage "); Serial.print(batteryVoltage()); Serial.print("V\t");
-
-  Serial.println(" ");
   loop_cnt++;
+  pixels.show(); // This sends the updated pixel color to the hardware.
+  delay(20);
+
 }
 
 
---
+++ src/arduhoop/led_functions.ino
@@ -0,0 +1,14 @@
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
