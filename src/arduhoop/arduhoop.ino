73249980a4b9b7b9e0462776a1068505aebb88e5
Arthur Benemann <arthur.benemann@gmail.com>
2016-05-07 15:23:27 -0700

    auto-formating

--- src/arduhoop/arduhoop.ino
+++ src/arduhoop/arduhoop.ino
@@ -9,34 +9,32 @@ int audio;
 
 void loop() {
 
-    Serial.print("Time "); Serial.print(millis()); Serial.print("ms\t");
+  Serial.print("Time "); Serial.print(millis()); Serial.print("ms\t");
 
-    // read raw accel/gyro measurements from device
-    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-    //accelgyro.getAcceleration(&ax, &ay, &az);
-    //accelgyro.getRotation(&gx, &gy, &gz);
+  // read raw accel/gyro measurements from device
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  //accelgyro.getAcceleration(&ax, &ay, &az);
+  //accelgyro.getRotation(&gx, &gy, &gz);
 
-    gx = abs(gx);
-    gy = abs(gy);
-    gz = abs(gz);
+  gx = abs(gx);
+  gy = abs(gy);
+  gz = abs(gz);
 
-    Serial.print("Acc:\t");
-    Serial.print(gx); Serial.print("\t");
-    Serial.print(gy); Serial.print("\t");
-    Serial.print(gz); Serial.print("\t");
+  Serial.print("Acc:\t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.print(gz); Serial.print("\t");
 
-    audio = readMic();
-    Serial.print("Audio\t"); Serial.print(audio); Serial.print("\t");
+  audio = readMic();
+  Serial.print("Audio\t"); Serial.print(audio); Serial.print("\t");
 
-    Serial.print("SW "); Serial.print(isButtonPressed() ? "[X]" : "[ ]"); Serial.print("\t");
-    Serial.print("SD_SW "); Serial.print(isSDcardPresent() ? "[X]" : "[ ]"); Serial.print("\t");
-    Serial.print("Charging "); Serial.print(isBatteryCharging() ? "[X]" : "[ ]"); Serial.print("\t");
-    Serial.print("Voltage "); Serial.print(batteryVoltage()); Serial.print("V\t");
+  Serial.print("SW "); Serial.print(isButtonPressed() ? "[X]" : "[ ]"); Serial.print("\t");
+  Serial.print("SD_SW "); Serial.print(isSDcardPresent() ? "[X]" : "[ ]"); Serial.print("\t");
+  Serial.print("Charging "); Serial.print(isBatteryCharging() ? "[X]" : "[ ]"); Serial.print("\t");
+  Serial.print("Voltage "); Serial.print(batteryVoltage()); Serial.print("V\t");
 
-    Serial.println(" ");
-    loop_cnt++;
-
-
+  Serial.println(" ");
+  loop_cnt++;
 
   for (int i = 0; i < led_number_pixels; i++) {
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
